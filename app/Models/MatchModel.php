4255db4512ac2bd0792d03a664b89fab7422fb5b
DD150602 <ljmdylan.delgado1011@gmail.com>
2024-08-29 00:00:38 -0500

    feat(Models): :sparkles: Recreate create match method
    this method is modified in order to be able to insert the referees at the same time

--- app/Models/MatchModel.php
+++ app/Models/MatchModel.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 use CodeIgniter\Model;
+use App\Models\RefereeModel;
+use App\Models\MatcheRefereeModel;
 
 class MatchModel extends Model
 {
@@ -12,9 +14,35 @@ class MatchModel extends Model
   protected $returnType = 'object';
   protected $allowedFields = ['match_date', 'match_hour', 'match_tournament_id', 'match_state', 'match_annotation'];
 
-  public function createMatch($data)
+  private $refereeModel;
+  private $matcheRefereeModel;
+
+  public function __construct()
   {
-    $this->insert($data);
+    $this->refereeModel = new RefereeModel();
+    $this->matcheRefereeModel = new MatcheRefereeModel();
+    
+  }
+
+  public function createMatchWithReferee($data)
+  {
+    $db = \Config\Database::connect();
+    $db->transStart();
+
+
+    $this->insert($data['matchData']);
+    $match_id = $db->insertID();
+
+    foreach ($data['refereesData'] as $referee) {
+      $referee_id = $this->refereeModel->getRefereeById($referee['referee_id'])->referee_id;
+
+      $this->matcheRefereeModel->insert([
+        'match_id' => $match_id,
+        'referee_id' => $referee_id
+      ]);
+    }
+
+    $db->transComplete();
   }
 
   public function updateMatch($data)
