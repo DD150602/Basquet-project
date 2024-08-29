5ecae2f093d1d4887de9d945e3a3a57d790bcf9c
DD150602 <ljmdylan.delgado1011@gmail.com>
2024-08-29 11:10:04 -0500

    chore(Models): :boom: Add return type

--- app/Models/MatcheRefereeModel.php
+++ app/Models/MatcheRefereeModel.php
@@ -9,4 +9,5 @@ class MatcheRefereeModel extends Model
   protected $table = 'matches_has_referees';
   protected $primaryKey = 'match_referee_id';
   protected $allowedFields = ['match_id', 'referee_id'];
+  protected $returnType = 'object';
 }
