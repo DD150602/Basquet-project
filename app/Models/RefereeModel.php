1a149531ff78c4f6065a149b72f443fe0795382b
DD150602 <ljmdylan.delgado1011@gmail.com>
2024-08-28 23:32:12 -0500

    feat(Models): :sparkles: Add model for referees

---
+++ app/Models/RefereeModel.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class RefereeModel extends Model
+{
+  protected $table = 'referees';
+  protected $primaryKey = 'referee_id';
+  protected $allowedFields = ['referee_name'];
+
+  public function getReferees()
+  {
+    return $this->findAll();
+  }
+
+  public function getRefereeById($id)
+  {
+    return $this->where('referee_id', $id)
+      ->find();
+  }
+
+  public function createReferee($data)
+  {
+    return $this->insert($data);
+  }
+
+  public function updateReferee($data)
+  {
+    return $this->where('referee_id', $data['referee_id'])
+      ->update($data);
+  }
+}
