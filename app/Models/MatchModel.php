77fda9dfe5b98db144acf961e23671befa8a9e76
DD150602 <ljmdylan.delgado1011@gmail.com>
2024-09-04 17:54:57 -0500

    fix(Models): :building_construction: refactor the model
    it's cahnged in order to fit the new database changes

--- app/Models/MatchModel.php
+++ app/Models/MatchModel.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 use App\Models\RefereeModel;
-use App\Models\MatcheRefereeModel;
 use App\Models\TeamMatchModel;
 
 class MatchModel extends Model
@@ -13,29 +12,20 @@ class MatchModel extends Model
   protected $primaryKey = 'match_id';
   protected $useAutoIncrement = true;
   protected $returnType = 'object';
-  protected $allowedFields = ['match_date', 'match_hour', 'match_tournament_id', 'match_state', 'match_annotation'];
+  protected $allowedFields = ['match_date', 'match_hour', 'tournament_id', 'match_state', 'match_annotation', 'match_description'];
 
   private $refereeModel;
-  private $matcheRefereeModel;
   private $teamsMatchModel;
 
   public function __construct()
   {
     $this->refereeModel = new RefereeModel();
-    $this->matcheRefereeModel = new MatcheRefereeModel();
     $this->teamsMatchModel = new TeamMatchModel();
-  }
-
-  public function getAllmatches()
-  {
-    return $this->select('match_id, match_date, match_hour, match_tournament_id')
-      ->where('match_state', true)
-      ->findAll();
   }
 
   public function getMatchById($id)
   {
-    return $this->select('match_id, match_date, match_hour, match_tournament_id')
+    return $this->select('match_id, match_date, match_hour, tournament_id, match_description')
       ->where('match_id', $id)
       ->where('match_state', true)
       ->find();
@@ -43,31 +33,15 @@ class MatchModel extends Model
 
   public function getMatchFromTournament($tournament_id)
   {
-    return $this->select('match_id, match_date, match_hour, match_tournament_id')
-      ->where('match_tournament_id', $tournament_id)
+    return $this->select('match_id, match_date, match_hour, tournament_id, match_description')
+      ->where('tournament_id', $tournament_id)
       ->where('match_state', true)
       ->findAll();
   }
 
   public function createMatchWithReferee($data)
   {
-    $db = \Config\Database::connect();
-    $db->transStart();
-
-
-    $this->insert($data['matchData']);
-    $match_id = $db->insertID();
-
-    foreach ($data['refereesData'] as $referee) {
-      $referee_id = $this->refereeModel->getRefereeById($referee['referee_id'])->referee_id;
-
-      $this->matcheRefereeModel->insert([
-        'match_id' => $match_id,
-        'referee_id' => $referee_id
-      ]);
-    }
-
-    $db->transComplete();
+    $this->insert($data);
   }
 
   public function assignTeamsToMatch($data)
@@ -81,6 +55,8 @@ class MatchModel extends Model
       $team_id = $team['team_id'];
 
       $this->teamsMatchModel->insert([
+        'match_team_points' => $team['match_team_points'],
+        'match_team_comments' => $team['match_team_comments'],
         'match_id' => $match_id,
         'team_id' => $team_id
       ]);
