3e6929be7143bec4013e405301cdd2f2ea7cab7c
DD150602 <ljmdylan.delgado1011@gmail.com>
2024-09-05 13:58:41 -0500

    chore(Models): :art: Change model in order to fit better
    Some fields are added
    methods to get the users are changed to show the name of its role
    the password is hashed when a user is created

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -10,18 +10,20 @@ class UserModel extends Model
   protected $primaryKey = 'user_id';
   protected $useAutoIncrement = true;
   protected $returnType = 'object';
-  protected $allowedFields = ['user_name', 'user_lastname', 'user_tshirt_number', 'user_role_id', 'user_team_id', 'user_state', 'user_annotation'];
+  protected $allowedFields = ['user_name', 'user_lastname', 'user_email', 'user_username', 'user_password', 'user_role_id', 'user_state', 'user_annotation'];
 
   public function getAllUsers()
   {
-    return $this->select('user_name, user_lastname, user_tshirt_number, user_role_id, user_team_id')
+    return $this->select('user_name, user_lastname, user_email, user_username, role_name, user_username')
+      ->join('roles', 'users.user_role_id = roles.role_id')
       ->where('user_state', true)
       ->findAll();
   }
 
   public function getUserById($id)
   {
-    return $this->select('user_name, user_lastname, user_tshirt_number, user_role_id, user_team_id')
+    return $this->select('user_name, user_lastname, user_email, user_username, role_name, user_username')
+      ->join('roles', 'users.user_role_id = roles.role_id')
       ->where('user_id', $id)
       ->where('user_state', true)
       ->find();
@@ -29,6 +31,8 @@ class UserModel extends Model
 
   public function createUser($data)
   {
+    $hashedPassword = password_hash($data['user_password'], PASSWORD_BCRYPT, ['cost' => 10]);
+    $data['user_password'] = $hashedPassword;
     $this->insert($data);
   }
 
@@ -38,7 +42,8 @@ class UserModel extends Model
     $this->update($data);
   }
 
-  public function deactivateUser($data){
+  public function deactivateUser($data)
+  {
     $this->where('user_id', $data['user_id']);
     $this->update($data);
   }
