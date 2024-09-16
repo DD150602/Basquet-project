cf86afea2f998f9161ca6aedb4fde6407560cb20
DD150602 <ljmdylan.delgado1011@gmail.com>
2024-09-16 10:28:30 -0500

    refactor(Views): :art: Add new admin folder with views

--- app/Controllers/Admin.php
+++ app/Controllers/Admin.php
@@ -6,41 +6,41 @@ class Admin extends BaseController
 {
   public function index()
   {
-    return view('Admin_page');
+    return view('Admin/Admin_page');
   }
 
   public function tournaments()
   {
-    return view('Tournaments_page');
+    return view('Admin/Tournaments_page');
   }
 
   public function insideTournaments()
   {
-    return view('Inside_tournaments_page');
+    return view('Admin/Inside_tournaments_page');
   }
 
   public function teams()
   {
-    return view('Team_page');
+    return view('Admin/Team_page');
   }
 
   public function players()
   {
-    return view('Player_page');
+    return view('Admin/Player_page');
   }
 
   public function referees()
   {
-    return view('Referees_page');
+    return view('Admin/Referees_page');
   }
 
   public function calendarManagement()
   {
-    return view('Match_Calendar_Managemen_page');
+    return view('Admin/Match_Calendar_Managemen_page');
   }
 
   public function resultsTracking()
   {
-    return view('Results_Tracking_page');
+    return view('Admin/Results_Tracking_page');
   }
 }
