c408981b0352ea5b9639fc4bf50a9e788d6c1425
DD150602 <ljmdylan.delgado1011@gmail.com>
2024-09-16 16:45:01 -0500

    feat(Views): :lipstick: Add new layout
    A layout is added along with some components

---
+++ app/Views/Templates/Components/Sidebar.php
@@ -0,0 +1,26 @@
+  <!-- Sidebar -->
+  <aside class="col-md-3 col-lg-2 bg-light p-4 shadow-sm">
+    <div class="d-flex align-items-center mb-4">
+      <span class="fw-bold h5">B.T.S</span>
+    </div>
+
+    <nav class="nav flex-column">
+      <!-- Dashboard -->
+      <a href="#" class="nav-link text-dark py-2">Dashboard</a>
+
+      <!-- Tournaments -->
+      <a href="<?php echo base_url('/admin/tournaments') ?>" class="nav-link text-dark py-2 active">Tournaments</a>
+
+      <!-- Teams -->
+      <a href="#" class="nav-link text-dark py-2">Teams</a>
+
+      <!-- Players -->
+      <a href="#" class="nav-link text-dark py-2">Players</a>
+
+      <!-- Referees -->
+      <a href="#" class="nav-link text-dark py-2">Referees</a>
+
+      <!-- Settings -->
+      <a href="#" class="nav-link text-dark py-2">Settings</a>
+    </nav>
+  </aside>
---
+++ app/Views/Templates/Footer.php
@@ -0,0 +1,5 @@
+
+<script src="<?php echo base_url('Resources/js/bootstrap.bundle.min.js') ?>"></script>
+</body>
+
+</html>
